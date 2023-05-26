ef5c8105cfdf6ddc7c7879a1719253c2479631a7
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-05-26 17:06:21 -0300

    values, names and colors randomizing

--- src/TrabajoPromocion/BurbujasEnAccion.java
+++ src/TrabajoPromocion/BurbujasEnAccion.java
@@ -8,44 +8,36 @@ public class BurbujasEnAccion {
         Scanner sc = new Scanner(System.in);
 
         // Declaración y asignación de variables
-        int resultadoUsuario1 = 0, resultadoUsuario2 = 0, valorBola1Usuario1 = 0, valorBola2Usuario1 = 0, valorBola1Usuario2 = 0, valorBola2Usuario2 = 0;
+        int resultadoUsuario1 = 0, resultadoUsuario2 = 0, valorBola1Usuario1, valorBola2Usuario1, valorBola1Usuario2, valorBola2Usuario2;
         String nombreUsuario1, nombreUsuario2, colorBola1Usuario1, colorBola2Usuario1, colorBola1Usuario2, colorBola2Usuario2, mensajeResultado;
         boolean bolasMismoColorUsuario1, bolasMismoColorUsuario2;
 
-        // Ingreso y lectura de valores
-        System.out.print("Ingrese el nombre del usuario 1: ");
-        nombreUsuario1 = sc.next();
-        System.out.print("Ingrese el nombre del usuario 2: ");
-        nombreUsuario2 = sc.next();
+        // Asignación de nombre aleatorios de usuarios
+        nombreUsuario1 = nombreAleatorio();
+        nombreUsuario2 = nombreAleatorio();
 
-        System.out.println(" ");
+        /*
+        * Asignación de valor aleatorio para bolas de usuarios
+        * Serán aleatorios en rango de 0 a 15
+        */
+        valorBola1Usuario1 = intAleatorio(0, 15);
+        valorBola2Usuario1 = intAleatorio(0, 15);
+        valorBola1Usuario2 = intAleatorio(0, 15);
+        valorBola2Usuario2 = intAleatorio(0, 15);
 
-        System.out.print("Ingrese el valor de la bola 1 para " + nombreUsuario1 + ": ");
-        valorBola1Usuario1 = sc.nextInt();
-        System.out.print("Ingrese el color de la bola 1 para " + nombreUsuario1 + ": ");
-        colorBola1Usuario1 = sc.next();
-        System.out.print("Ingrese el valor de la bola 2 para " + nombreUsuario1 + ": ");
-        valorBola2Usuario1 = sc.nextInt();
-        System.out.print("Ingrese el color de la bola 2 para " + nombreUsuario1 + ": ");
-        colorBola2Usuario1 = sc.next();
+        /*
+        * Asignación de color aleatorio para bolas de usuarios
+        */
+        colorBola1Usuario1 = colorAleatorio();
+        colorBola2Usuario1 = colorAleatorio();
+        colorBola1Usuario2 = colorAleatorio();
+        colorBola2Usuario2 = colorAleatorio();
 
-
-        System.out.println(" ");
-
-        System.out.print("Ingrese el valor de la bola 1 para " + nombreUsuario2 + ": ");
-        valorBola1Usuario2 = sc.nextInt();
-        System.out.print("Ingrese el color de la bola 1 para " + nombreUsuario2 + ": ");
-        colorBola1Usuario2 = sc.next();
-        System.out.print("Ingrese el valor de la bola 2 para " + nombreUsuario2 + ": ");
-        valorBola2Usuario2 = sc.nextInt();
-        System.out.print("Ingrese el color de la bola 2 para " + nombreUsuario2 + ": ");
-        colorBola2Usuario2 = sc.next();
-
-
-
+        // Llamo al módulo correspondiente para validar si las bolas son del mismo color
         bolasMismoColorUsuario1 = validarMismoColor(colorBola1Usuario1, colorBola2Usuario1);
         bolasMismoColorUsuario2 = validarMismoColor(colorBola1Usuario2, colorBola2Usuario2);
 
+        // Si las bolas son iguales, calculo el puntaje del usuario
         if (bolasMismoColorUsuario1) {
             resultadoUsuario1 = calcularPuntaje(valorBola1Usuario1, valorBola2Usuario1);
         }
@@ -79,6 +71,52 @@ public class BurbujasEnAccion {
         }
 
         return mensaje;
+    }
+
+    public static double doubleAleatorio(int min, int max){
+        return Math.random() * (max-min) + min;
+    }
+
+    public static int intAleatorio(int min, int max){
+        return (int) doubleAleatorio(min, max);
+    }
+
+    public static char charAleatorio(){
+        char a;
+
+        if (Math.random() >= 0.5){
+            // letras mayúsculas
+            a = (char) intAleatorio(97, 123);
+        } else {
+            // letras minúsculas
+            a = (char) intAleatorio(65, 91);
+        }
+
+        return a;
+    }
+
+    public static String stringAleatorio(int tam){
+        String res = "";
+
+        for(int i = 0; i < tam; i++){
+            res = res + charAleatorio();
+        }
+
+        return res;
+    }
+
+    public static String nombreAleatorio(){
+        // ejemplo para definir un valor aleatorio entre varias posibilidades
+        // en este caso nombres
+        String arrayNombres[] = {"Anastasia", "Florencia", "Regina", "Rocío", "Agostina", "Lucía", "Rigoberto", "Juan", "Carlos", "Pedro", "Tito", "Cacho"};
+        return arrayNombres[intAleatorio(0, arrayNombres.length - 1)];
+    }
+
+    public static String colorAleatorio(){
+        // ejemplo para definir un valor aleatorio entre varias posibilidades
+        // en este caso colores
+        String arrayColores[] = {"rojo", "verde", "azul", "amarillo", "celeste", "rosado"};
+        return arrayColores[intAleatorio(0, arrayColores.length - 1)];
     }
 
 }
