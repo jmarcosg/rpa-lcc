b319c319b3faf56fb8844b2e4008d5121b929d06
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-05-30 18:46:57 -0300

    add functions comments

--- src/TrabajoPromocion/BurbujasEnAccion.java
+++ src/TrabajoPromocion/BurbujasEnAccion.java
@@ -53,17 +53,19 @@ public class BurbujasEnAccion {
     }
 
     public static boolean validarMismoColor(String color1, String color2) {
+        // valida si dos colores son iguales
         return color1.equals(color2);
     }
 
     public static int calcularPuntaje(int valor1, int valor2) {
+        // calcula el puntaje de un usuario
         int resultado;
 
         // Calculo el resultado parcial
         resultado = valor1 + valor2;
 
-        // Reglas para puntajes
         /*
+        * Reglas para puntajes
         * Si la suma de los dos valores es par, entonces suma 10 puntos extra
         * Si la suma de los dos valores es impar y menor a 10, entonces duplica su puntaje */
         if (resultado % 2 == 0) {
@@ -76,6 +78,7 @@ public class BurbujasEnAccion {
     }
 
     public static String calcularGanador(String nombreUsuario1, int resultadoUsuario1, String nombreUsuario2, int resultadoUsuario2 ) {
+        // calcula quien ganó la partida
         String mensaje;
 
         if (resultadoUsuario1 > resultadoUsuario2) {
